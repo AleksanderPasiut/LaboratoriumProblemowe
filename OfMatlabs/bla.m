1b7213ebf4f84db9142b4136c9ddddd42882f5b9
andrzejbro <andrzej.bro@interia.pl>
2018-04-18 10:17:31 +0200

    Add files via upload

--- OfMatlabs/bla.m
+++ OfMatlabs/bla.m
@@ -1,57 +1,36 @@
 clear all
 close all
 
-trzy_palce = 0.5*0.8354;
+kX = 0.2;
+kY = 0.2;
 
-% there and back again
-% utx = [0.8 4 7 10];
-% ux = [0.2 -0.4 0.2 0];
-% 
-% uty = [0.8 4 7 10];
-% uy = [0 0.2 -0.4 0.2];
+eXscale = 2;
+eYscale = 2;
+alphaXscale = 1;
 
-% uty = [0.8 2 3];
-% uy = [0.2 -0.4 0.2];
+for alphaYscale = 1:3
+    
+    BasicTestFunction( 'GoHomeXYZ', 0.5, 'Go Home !!!' );
+    pause(10)
+    set_param('Crane3D_DevDriv', 'SimulationCommand', 'start')
 
-% utx = [1 2 3];
-% ux = [0 0 0];
-
-% there and stop
-utx = [1 3 10];
-ux = [0.5 -0.5 0];
-
-uty = [1 3 10];
-uy = [0.5 -0.5 0];
-
-% uty = [1 2 3];
-% uy = [0 0 0];
-
-% utx = [1 2 3];
-% ux = [0 0 0];
-
-
-
-BasicTestFunction( 'GoHomeXYZ', 0.5, 'Go Home !!!' );
-pause(10)
-set_param('Crane3D_DevDriv', 'SimulationCommand', 'start')
-
-temp = exist('positions_and_angles');
-while temp ~= 1
     temp = exist('positions_and_angles');
-    pause(1)
+    while temp ~= 1
+        temp = exist('positions_and_angles');
+        pause(1)
+    end
+
+    figure;
+    plot(time, positions_and_angles)
+    legend('x','y','x angle','y angle')
+    title(alphaYscale);
+
+    figure;
+    plot(time, [eXplot, eYplot]);
+    title(alphaYscale);
+    
+    clear positions_and_angles
+    
 end
 
-plot(time,positions_and_angles)
-legend('x','y','x angle','y angle')
 
-formatSpec='%f %f %f %f %f %f %f %f \r\n';
-size = 8000;
-
-data_out(1,:) = time';
-data_out(2:3,:) = control';
-data_out(4:7,:) = positions_and_angles';
-data_out(8,:) = z';
-
-file = fopen('out.txt','w');
-fprintf(file,formatSpec,data_out);
-fclose(file);
